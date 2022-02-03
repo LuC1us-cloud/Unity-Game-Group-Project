6e25f6a79b6ca3755dc82e1a72098ba5631dc13f
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-02-03 21:34:26 +0200

    Added Item.cs and Mainplayer.cs

---
+++ Assets/Entity/Friendly/Main Player/MainPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : Entity
+{
+    public List<Item> inventory = new List<Item>();
+    public Item Head;
+    public Item Chest;
+    public Item Legs;
+    public Item Feet;
+    public Item Hands;
+    public Item Weapon;
+    public Item Shield;
+    public Item Ring;
+    public Item Necklace;
+    public Item Amulet;
+    public Item Belt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Items/Item.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public string name;
+    public string description;
+    public int weight;
+    public int value;
+    public int quantity;
+    public Sprite sprite;
+    public bool stackable;
+    public bool consumable;
+    public bool usable;
+    public bool equippable;
+    public bool questItem;
+    public bool isEquipped;
+}

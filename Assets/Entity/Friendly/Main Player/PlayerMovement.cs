5cb0dc1257c58f39b4dc81e553f8b36c581c0423
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-02-05 00:38:31 +0200

    Added specialAbility to MainPlayer

--- Assets/Entity/Friendly/Main Player/PlayerMovement.cs
+++ Assets/Entity/Friendly/Main Player/PlayerMovement.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public bool isMovementRelativeToCamera = false;
+    public enum DoubleShiftUsage
+    {
+        None,
+        Dash,
+    }
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Camera cam;
-    
+    [Header("Double Shift Usage")]
+    public DoubleShiftUsage specialAbility = DoubleShiftUsage.None;
+    public float dashDistance = 20f;
+
     Vector2 mousePos;
     Vector2 movement;
+    bool shiftClickedOnce = false;
+    bool shiftClickedTwice = false;
+    float shiftClickInterval = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (shiftClickedOnce && Input.GetKeyDown(KeyCode.LeftShift) && !shiftClickedTwice && 0 < shiftClickInterval)
+        {
+            shiftClickedTwice = true;
+            shiftClickInterval = 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            shiftClickedOnce = true;
+        }
+        if (shiftClickedOnce && !shiftClickedTwice)
+        {
+            shiftClickInterval -= Time.deltaTime;
+            if (shiftClickInterval <= 0)
+            {
+                shiftClickedOnce = false;
+                shiftClickedTwice = false;
+                shiftClickInterval = 0.5f;
+            }
+        }
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
@@ -26,15 +56,21 @@ public class PlayerMovement : MonoBehaviour
         float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
 
-        if (isMovementRelativeToCamera)
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (shiftClickedTwice)
         {
-            movement.x = (1f * Mathf.Cos(angle * Mathf.Deg2Rad));
-            movement.y = (1f * Mathf.Sin(angle * Mathf.Deg2Rad));
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-        }
-        else
-        {
-            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+            switch (specialAbility)
+            {
+                case DoubleShiftUsage.Dash:
+                    rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime * dashDistance);
+                    break;
+                
+                default:
+                    break;
+            }
+            shiftClickedOnce = false;
+            shiftClickedTwice = false;
         }
     }
 }

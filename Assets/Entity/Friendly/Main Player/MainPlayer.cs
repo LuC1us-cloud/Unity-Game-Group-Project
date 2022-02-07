c788442958805064e40f6b81d8d4f879e9d406c1
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-02-08 01:28:53 +0200

    Inventory UI and logic

--- Assets/Entity/Friendly/Main Player/MainPlayer.cs
+++ Assets/Entity/Friendly/Main Player/MainPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MainPlayer : Entity
 {
     public List<Item> inventory = new List<Item>();
+    public GameObject inventoryUI;
     // public bool isInventoryOpen = false;
     // public GameObject inventoryUI;
     private Item Head;
@@ -20,24 +21,85 @@ public class MainPlayer : Entity
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if tab is pressed toggle inventory GameObject visibility
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            inventoryUI.SetActive(!inventoryUI.activeSelf);
+            // and pause the game
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        }
+        if (Time.timeScale == 0) return;
+
+        // if e is pressed call function called use()
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Use();
+        }
         if (Input.GetButton("Fire1"))
         {
-            if(rightHand != null){
-                rightHand.GetComponent<RangedWeapon>().Shoot();
-            }
+            if (rightHand == null) return;
+            rightHand.GetComponent<RangedWeapon>().Shoot();
         }
         if (Input.GetButton("Fire2"))
         {
-            if(leftHand != null){
-                leftHand.GetComponent<RangedWeapon>().Shoot();
+            if (leftHand == null) return;
+            leftHand.GetComponent<RangedWeapon>().Shoot();
+        }
+    }
+    void Use()
+    {
+        // if the GameObject directly under the cursor has an Interactable component
+        // and that GameObject is in radius of 1.5f of the MainPlayer, call the Interact() function
+        // else search for the nearest Item and call the PickUp() function
+
+        // get the GameObject under the cursor, by raycasting from cursor position
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        if (hit.collider != null)
+        {
+            // check if the GameObject under the cursor is closer than 1.5f to the MainPlayer
+            if (Vector2.Distance(transform.position, hit.collider.transform.position) > 1.5f) return;
+            // if the GameObject under the cursor has an Interactable component
+            if (hit.collider.GetComponent<Interactable>() == null) return;
+            // call the Interact() function
+            hit.collider.GetComponent<Interactable>().Interact();
+        }
+        else
+        {
+            // get all gameObjects with tag "Item" in radius of 1.5f of the MainPlayer
+            Collider2D[] items = Physics2D.OverlapCircleAll(transform.position, 1.5f);
+            // loop through all items
+            GameObject closest = null;
+            foreach (Collider2D item in items)
+            {
+                // if the item is an Item
+                if (item.gameObject.tag == "Item")
+                {
+                    // if the closest item is null or the item is closer than the closest item
+                    if (closest == null ||
+                    Vector2.Distance(transform.position, item.transform.position) <
+                    Vector2.Distance(transform.position, closest.transform.position))
+                    {
+                        // set the closest item to the item
+                        closest = item.gameObject;
+                    }
+                }
+            }
+            // if the closest item is not null
+            if (closest != null)
+            {
+                // call the PickUp() function
+                PickUp(closest);
             }
         }
-        
+    }
+    void PickUp()
+    {
+
     }
 }

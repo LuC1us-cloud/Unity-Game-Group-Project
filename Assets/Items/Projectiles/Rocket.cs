e03ab0f8834a4baa79bba708f586ab896b34a03e
matas108 <m.dzingulevicius@gmail.com>
2022-02-09 16:16:45 +0200

    Rocket launcher
    Added rocket launcher  gun and bullet

---
+++ Assets/Items/Projectiles/Rocket.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : Projectile
+{
+    public float explosionRadius;
+    public LayerMask layers;
+
+    private void FixedUpdate()
+    {
+        // if bullet is alive for too long, destroy it
+        timeAlive += Time.deltaTime;
+        if (timeAlive >= timeToLive)
+        {
+            Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, explosionRadius, layers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                Debug.Log("hit");
+                if (enemy.gameObject.tag == "Enemy")
+                {
+                    enemy.gameObject.GetComponent<Entity>().TakeDamage(damage);
+                }
+                if (enemy.gameObject.tag == "MageEnemy")
+                {
+                    enemy.gameObject.GetComponent<mageEnemy>().TakeDamage(damage);
+                }
+            }
+            
+            Destroy(gameObject);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, explosionRadius, layers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("hit" + enemy.name);
+            if (enemy.gameObject.tag == "Enemy")
+            {
+                enemy.gameObject.GetComponent<Entity>().TakeDamage(damage);
+            }
+            if (enemy.gameObject.tag == "MageEnemy")
+            {
+                enemy.gameObject.GetComponent<mageEnemy>().TakeDamage(damage);
+            }
+        }
+        
+        Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}

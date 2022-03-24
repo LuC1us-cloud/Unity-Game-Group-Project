0e2edf384430a919e0be51c1cbbb9642887a4c14
LuC1us-cloud <55689544+LuC1us-cloud@users.noreply.github.com>
2022-03-24 17:47:30 +0200

    StatPanel changes

--- Assets/GUI/Overlay/InspectEntityText.cs
+++ Assets/GUI/Overlay/InspectEntityText.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Text;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,8 @@ public class InspectEntityText : MonoBehaviour
 {
     // Start is called before the first frame update
     // a public text-mesh pro element reference
-    public TMPro.TextMeshProUGUI text;
+    public TMPro.TextMeshProUGUI textStats;
+    public TMPro.TextMeshProUGUI textName;
     private GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,10 @@ public class InspectEntityText : MonoBehaviour
     }
 
     // Update is called once per frame
+    int health;
+    int armor;
+    int damage;
+    float timePast = 0;
     void Update()
     {
         GameObject entity;
@@ -31,12 +37,41 @@ public class InspectEntityText : MonoBehaviour
         {
             entity = hit.collider.gameObject;
         }
-
-        string textToDisplay = $"{entity.gameObject.name}\n";
-        textToDisplay += "Health: " + entity.GetComponent<Entity>().CurrentHealth.ToString() + "\n";
-        textToDisplay += "Armor: " + entity.GetComponent<Entity>().Armor.ToString() + "\n";
-        textToDisplay += "Damage: " + entity.GetComponent<Entity>().Damage.ToString() + "\n";
+        // if the new values are different, lerp to them gradually
+        int currentHealth = 0;
+        int currentArmor = 0;
+        int currentDamage = 0;
+        if (entity.GetComponent<Entity>().Armor != armor || entity.GetComponent<Entity>().CurrentHealth != health || entity.GetComponent<Entity>().Damage != damage)
+        {
+            currentHealth = (int)Mathf.LerpUnclamped(health, entity.GetComponent<Entity>().CurrentHealth, timePast);
+            currentArmor = (int)Mathf.LerpAngle(armor, entity.GetComponent<Entity>().Armor, timePast);
+            currentDamage = (int)Mathf.Lerp(damage, entity.GetComponent<Entity>().Damage, timePast);
+            timePast += Time.deltaTime * 10;
+        }
+        // if all values are equal, health, armor, damage to the current values
+        else
+        {
+            health = currentHealth;
+            armor = currentArmor;
+            damage = currentDamage;
+            timePast = 0;
+        }
         
-        text.text = textToDisplay;
+        StringBuilder textToDisplay = new StringBuilder($"Health: {currentHealth}\n");
+        textToDisplay.Append($"Armor: {currentArmor}\n");
+        textToDisplay.Append($"Damage: {currentDamage}\n");
+        
+        textStats.text = textToDisplay.ToString();
+        textName.text = entity.gameObject.name;
+    }
+
+    // lerp to a new value gradually
+    IEnumerator LerpTo(string text, string newText)
+    {
+        for (float i = 0; i < 1; i += Time.deltaTime)
+        {
+            textStats.text = Mathf.Lerp(float.Parse(text), float.Parse(newText), i).ToString();
+            yield return null;
+        }
     }
 }

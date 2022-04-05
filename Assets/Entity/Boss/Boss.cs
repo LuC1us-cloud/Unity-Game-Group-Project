7bd98032dbac561e73b876b216934481af9b730e
Ropke13 <79408886+Ropke13@users.noreply.github.com>
2022-04-05 16:46:05 +0300

    BossBattle
    Added Boss Shoting, fixed collisions and stuff.

--- Assets/Entity/Boss/Boss.cs
+++ Assets/Entity/Boss/Boss.cs
@@ -11,6 +11,7 @@ public class Boss : MonoBehaviour
     public Rigidbody2D rb;
     public Transform wallCheckPos;
     public LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,18 +27,13 @@ public class Boss : MonoBehaviour
         }
     }
 
-    //private void FixedUpdate(){
-     //   if(isPatroling){
-    //        flip = Physics2D.OverlapCircle(wallCheckPos.position, 0.1f, groundLayer);
-    //    }
-  //  }
-
     void Patrol(){
         if(flip){
             Flip();
         }
         rb.velocity = new Vector2(Speed * Time.fixedDeltaTime, rb.velocity.y);
     }
+
     void Flip(){
         isPatroling = false;
         transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
